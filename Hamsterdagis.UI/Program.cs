87181b4332caa78b74f2f78839920f35b3aa5401
Louis122333 <74004258+Louis122333@users.noreply.github.com>
2021-03-31 17:30:18 +0200

    Add files via upload

---
+++ Hamsterdagis.UI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hamsterdagis.UI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            UserInterface userInterface = new UserInterface();
+            userInterface.Menu();
+            
+            
+            Console.ReadLine();
+
+        }
+        
+        
+    }
+   
+}
+       
+
+    
+
---
+++ Hamsterdagis.UI/UserInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Hamsterdagis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamsterdagis.UI
+{
+    public class UserInterface
+    {
+        
+        public void Menu()
+        {
+            
+            HamsterDayCare h = new HamsterDayCare();
+            
+            HamsterDayCare.ReadFile();
+            HamsterDayCare.PrintHamsters();
+
+
+        }
+    }
+}
